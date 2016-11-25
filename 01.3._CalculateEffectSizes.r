71532f66e9f9a3fb46ccca1cc43765f4c2f8903f
KatharinaGerstner <katharina.gerstner@idiv.de>
2016-11-25 15:28:11 +0100

    Adapt names of variables

--- 01.3._CalculateEffectSizes.r
+++ 01.3._CalculateEffectSizes.r
@@ -1,30 +1,20 @@
-div.df <- read.csv(path2temp %+% "DiversityData.csv", sep=";")
-# div.df <- read.csv(path2Dropbox %+% "Analysis/DiversityData.csv",sep=";")
+div_df <- read.csv(path2temp %+% "DiversityData.csv", sep=";")
 
-#table(div.df$filename)
-# table(div.df$entity.id)
-
-### get rid of irrelevant data, e.g. matrix, clearcut
-div.df <- subset(div.df, entity.size.rank >0)
-
+str(div_df)
 ### calculate rank-correlation
-ES.df <- data.frame(Study.ID=unique(div.df$filename))
-names.df <- c("n.fragment",c("z.","z.var.") %+% names(div.df)[-(1:3)])
-ES.df[,names.df] <- NA
+ES_df <- data.frame(Study.ID=unique(div_df$filename))
+namES_df <- c("n.fragment",c("z.","z.var.") %+% rep(names(div_df)[-(1:3)],each=2))
+ES_df[,namES_df] <- NA
 
-for(i in 1:length(ES.df$Study.ID)){
-   sub.df <- subset(div.df, filename==ES.df$Study.ID[i])
-   for(j in names(div.df)[-(1:3)]){
-      ES.df[i,"n.fragment"] <- nrow(sub.df)
-      r <- ifelse(ES.df[i,"n.fragment"]<90,2*sin(pi*cor(sub.df$entity.size.rank,sub.df[,j])/6),cor(sub.df$entity.size.rank,sub.df[,j])) ### transform rank-correlation into Pearson-correlation, cf. Box 13.3. p 201 in Koricheva et al 2013
-      ES.df[i,"z." %+% j] <- 1/2*log((1+r)/(1-r))
-      ES.df[i,"z.var." %+% j] <- ifelse(ES.df[i,"n.fragment"]>3,1/(ES.df[i,"n.fragment"]-3),NA)
+for(i in 1:length(ES_df$Study.ID)){
+   sub.df <- subset(div_df, filename==ES_df$Study.ID[i])
+   for(j in names(div_df)[-(1:3)]){
+      ES_df[i,"n.fragment"] <- nrow(sub.df)
+      r <- ifelse(ES_df[i,"n.fragment"]<90,2*sin(pi*cor(sub.df$entity.size.rank,sub.df[,j])/6),cor(sub.df$entity.size.rank,sub.df[,j])) ### transform rank-correlation into Pearson-correlation, cf. Box 13.3. p 201 in Koricheva et al 2013
+      ES_df[i,"z." %+% j] <- 1/2*log((1+r)/(1-r))
+      ES_df[i,"z.var." %+% j] <- ifelse(ES_df[i,"n.fragment"]>3,1/(ES_df[i,"n.fragment"]-3),NA)
    }
 }
 
-write.csv(ES.df, file=path2temp %+% "ES.df.csv")
+write.csv(ES_df, file=path2temp %+% "ES_df.csv")
 
-### forest plots
-pdf(path2temp %+% "ForestPlots.pdf")
-sapply(names(div.df)[-(1:3)], function(j) forest(x=ES.df[,"z." %+% j],vi=ES.df[,"z.var." %+% j],slab=ES.df$Study.ID,psize=1,main=paste(j),cex=.6))
-dev.off()       
