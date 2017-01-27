9d14e5141594fa3287b68151367173e22f1edd37
KatharinaGerstner <katharina.gerstner@idiv.de>
2017-01-27 14:27:05 +0100

    adapt structure of descriptive plots

--- 02.2_DescriptiveStats.r
+++ 02.2_DescriptiveStats.r
@@ -1,5 +1,5 @@
 load(path2temp %+% "Data4Analysis.Rdata") # ES_df.complete, df.complete
-BDmetrics <- c("N", "S", "S_cov", "PIE")
+BDmetrics <- c("N", "S", "ENS_pie", "Pielou_even")
 
 ############################################################################
 ### 1. Plot effect sizes in a 2-dim space
@@ -11,34 +11,34 @@ ggplot(data=ES_df.complete, aes(x=z.N,y=z.S,col=taxa)) +
    geom_errorbarh(aes(xmin=z.N - (1.96*sqrt(z.var.N)),xmax=z.N + (1.96*sqrt(z.var.N)))) +
    geom_abline(intercept=0,slope=1,lty="dotted")
 
-ggplot(data=ES_df.complete, aes(x=z.N,y=z.S_cov,col=taxa)) + 
+ggplot(data=ES_df.complete, aes(x=z.N,y=z.ENS_pie,col=taxa)) + 
    geom_point(alpha=.5,size=3) +
-   geom_errorbar(aes(ymin=z.S_cov - (1.96*sqrt(z.var.S_cov)),ymax=z.S_cov + (1.96*sqrt(z.var.S_cov)))) +
+   geom_errorbar(aes(ymin=z.ENS_pie - (1.96*sqrt(z.var.ENS_pie)),ymax=z.ENS_pie + (1.96*sqrt(z.var.ENS_pie)))) +
    geom_errorbarh(aes(xmin=z.N - (1.96*sqrt(z.var.N)),xmax=z.N + (1.96*sqrt(z.var.N)))) +
    geom_abline(intercept=0,slope=1,lty="dotted")
 
-ggplot(data=ES_df.complete, aes(x=z.N,y=z.PIE,col=taxa)) + 
+ggplot(data=ES_df.complete, aes(x=z.N,y=z.Pielou_even,col=taxa)) + 
    geom_point(alpha=.5,size=3) +
-   geom_errorbar(aes(ymin=z.PIE - (1.96*sqrt(z.var.PIE)),ymax=z.PIE + (1.96*sqrt(z.var.PIE)))) +
+   geom_errorbar(aes(ymin=z.Pielou_even - (1.96*sqrt(z.var.Pielou_even)),ymax=z.Pielou_even + (1.96*sqrt(z.var.Pielou_even)))) +
    geom_errorbarh(aes(xmin=z.N - (1.96*sqrt(z.var.N)),xmax=z.N + (1.96*sqrt(z.var.N)))) +
    geom_abline(intercept=0,slope=1,lty="dotted")
 
-ggplot(data=ES_df.complete, aes(x=z.S,y=z.S_cov,col=taxa)) + 
+ggplot(data=ES_df.complete, aes(x=z.S,y=z.ENS_pie,col=taxa)) + 
    geom_point(alpha=.5,size=3) +
-   geom_errorbar(aes(ymin=z.S_cov - (1.96*sqrt(z.var.S_cov)),ymax=z.S_cov + (1.96*sqrt(z.var.S_cov)))) +
+   geom_errorbar(aes(ymin=z.ENS_pie - (1.96*sqrt(z.var.ENS_pie)),ymax=z.ENS_pie + (1.96*sqrt(z.var.ENS_pie)))) +
    geom_errorbarh(aes(xmin=z.S - (1.96*sqrt(z.var.S)),xmax=z.S + (1.96*sqrt(z.var.S)))) +
    geom_abline(intercept=0,slope=1,lty="dotted")
 
-ggplot(data=ES_df.complete, aes(x=z.S,y=z.PIE,col=taxa)) + 
+ggplot(data=ES_df.complete, aes(x=z.S,y=z.Pielou_even,col=taxa)) + 
    geom_point(alpha=.5,size=3) +
-   geom_errorbar(aes(ymin=z.PIE - (1.96*sqrt(z.var.PIE)),ymax=z.PIE + (1.96*sqrt(z.var.PIE)))) +
+   geom_errorbar(aes(ymin=z.Pielou_even - (1.96*sqrt(z.var.Pielou_even)),ymax=z.Pielou_even + (1.96*sqrt(z.var.Pielou_even)))) +
    geom_errorbarh(aes(xmin=z.S - (1.96*sqrt(z.var.S)),xmax=z.S + (1.96*sqrt(z.var.S)))) +
    geom_abline(intercept=0,slope=1,lty="dotted")
 
-ggplot(data=ES_df.complete, aes(x=z.S_cov,y=z.PIE,col=taxa)) + 
+ggplot(data=ES_df.complete, aes(x=z.ENS_pie,y=z.Pielou_even,col=taxa)) + 
    geom_point(alpha=.5,size=3) +
-   geom_errorbar(aes(ymin=z.PIE - (1.96*sqrt(z.var.PIE)),ymax=z.PIE + (1.96*sqrt(z.var.PIE)))) +
-   geom_errorbarh(aes(xmin=z.S_cov - (1.96*sqrt(z.var.S_cov)),xmax=z.S + (1.96*sqrt(z.var.S_cov)))) +
+   geom_errorbar(aes(ymin=z.Pielou_even - (1.96*sqrt(z.var.Pielou_even)),ymax=z.Pielou_even + (1.96*sqrt(z.var.Pielou_even)))) +
+   geom_errorbarh(aes(xmin=z.ENS_pie - (1.96*sqrt(z.var.ENS_pie)),xmax=z.S + (1.96*sqrt(z.var.ENS_pie)))) +
    geom_abline(intercept=0,slope=1,lty="dotted")
 dev.off()
 
@@ -46,7 +46,7 @@ dev.off()
 ### 1. Plot effect sizes in a 3-dim space
 ############################################################################
 pdf(file=path2temp %+% "3D_Scatterplot_of_z.pdf")
-with(ES_df.complete, scatter3D(x = z.N, y = z.S_cov, z = z.PIE, 
+with(ES_df.complete, scatter3D(x = z.N, y = z.S, z = z.ENS_pie, 
                                col = rainbow(length(levels(taxa))), 
                                pch = 16, cex = 1.5, alpha=0.6,
                                xlab = "Abundance N", ylab = "Species richness S", zlab = "ENS PIE", phi=5, ltheta=2, lphi=2,
@@ -55,7 +55,7 @@ with(ES_df.complete, scatter3D(x = z.N, y = z.S_cov, z = z.PIE,
                                colkey = F))
 legend("bottomleft", paste(levels(ES_df.complete$taxa)), pch = 16, col = rainbow(length(levels(ES_df.complete$taxa))), cex=1, inset=c(-0.01,-0.03),bty="n")
 
-with(ES_df.complete, scatter3D(x = z.N, z = z.S_cov, y = z.PIE, 
+with(ES_df.complete, scatter3D(x = z.N, z = z.S, y = z.ENS_pie, 
                                col = rainbow(length(levels(taxa))), 
                                pch = 16, cex = 1.5, alpha=0.6,
                                xlab = "Abundance N", zlab = "Species richness S", ylab = "ENS PIE", phi=5, ltheta=2, lphi=2,
@@ -64,7 +64,7 @@ with(ES_df.complete, scatter3D(x = z.N, z = z.S_cov, y = z.PIE,
                                colkey = F))
 legend("bottomleft", paste(levels(ES_df.complete$taxa)), pch = 16, col = rainbow(length(levels(ES_df.complete$taxa))), cex=1, inset=c(-0.01,-0.03),bty="n")
 
-with(ES_df.complete, scatter3D(z = z.N, y = z.S_cov, x = z.PIE, 
+with(ES_df.complete, scatter3D(z = z.N, y = z.S, x = z.ENS_pie, 
                                col = rainbow(length(levels(taxa))), 
                                pch = 16, cex = 1.5, alpha=0.6,
                                zlab = "Abundance N", ylab = "Species richness S", xlab = "ENS PIE", phi=5, ltheta=2, lphi=2,
@@ -99,9 +99,18 @@ panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor=1, ...)
    rect(0,0,1,1,col=col.vec)
    text(0.5, 0.5, txt, cex = cex.cor)
 }
-
-pairs(ES_df.complete[,"z." %+% BDmetrics], labels=BDmetrics,lower.panel = panel.smooth, upper.panel = panel.cor, cex.labels=0.6)
+panel.smooth <- function (x, y, col = par("col"), bg = NA, pch = par("pch"), 
+                          cex = 1, col.smooth = "red", span = 2/3, iter = 3, ...) 
+{
+   points(x, y, pch = pch, col = col, bg = bg, cex = cex)
+   ok <- is.finite(x) & is.finite(y)
+   if (any(ok)) 
+      lines(stats::lowess(x[ok], y[ok], f = span, iter = iter), 
+            col = col.smooth, ...)
+   abline(0,1,lty="dotted")
+}
+pairs(ES_df.complete[,"z." %+% BDmetrics], labels=BDmetrics,lower.panel = panel.smooth, upper.panel = panel.cor)
 
 png(file=path2temp %+% "PairwiseCorPlot.png", width=20,height=20,units="cm",res=400,type = "cairo-png")
-pairs(ES_df.complete[,"z." %+% BDmetrics], labels=BDmetrics,lower.panel = panel.smooth, upper.panel = panel.cor, cex.labels=0.6)
+pairs(ES_df.complete[,"z." %+% BDmetrics], labels=BDmetrics,lower.panel = panel.smooth, upper.panel = panel.cor)
 dev.off()
