cb8e8485a45a3f8259a3b4402e0cd274efa06073
KatharinaGerstner <katharina.gerstner@idiv.de>
2017-03-09 15:56:14 +0100

    calculate effect size for groups and gradients of fragments

--- 01.3_CalculateEffectSizes.r
+++ 01.3_CalculateEffectSizes.r
@@ -2,50 +2,54 @@ div_df <- read.csv(path2temp %+% "DiversityData.csv", sep=",")
 
 str(div_df)
 
+BDmetrics <- c("N_std","S","D0_hat","ENS_pie")
+
+###############################################################
 ### 1. largest vs smallest fragment incl continuous as fragment
+###############################################################
 ES_frag_df <- data.frame(Case.ID=unique(div_df$filename))
-namES_df <- c("n.fragment",c("logRR.") %+% names(div_df)[-(1:3)])
+namES_df <- c("n.fragment",c("logRR.") %+% BDmetrics)
 ES_frag_df[,namES_df] <- NA
-# small.df <- large.df <- ES_frag_df
-# small.df$type <- "small"
-# large.df$type <- "large"
 
 for(i in 1:length(ES_frag_df$Case.ID)){
    sub.df <- subset(div_df, filename %in% ES_frag_df$Case.ID[i])
    small.df <- sub.df[which.min(sub.df$entity.size.rank),]
    large.df <- sub.df[which.max(sub.df$entity.size.rank),]
-   for(j in names(div_df)[-(1:3)]){
-      ES_frag_df[i,"n.fragment"] <- nrow(sub.df)
+   ES_frag_df[i,"n.fragment"] <- nrow(sub.df)
+   for(j in BDmetrics){
       ES_frag_df[i,"logRR." %+% j] <- log(small.df[,j]/large.df[,j])
    }
 }
-#raw_frag_df <- rbind(small.df,large.df) # raw data of small vs large fragments to be analysed in a glmer-framework, cf Newbold et al. 2015
 
-### 2. largest vs smallest fragment group
+###############################################################
+### 2. largest (incl. continuous) vs smallest fragment group
+###############################################################
 ES_frag_group_df <- data.frame(Case.ID=unique(div_df$filename))
-namES_df <- c("n.fragment",c("logRR.") %+% rep(names(div_df)[-(1:3)],each=2))
+namES_df <- c("n.fragment",c("logRR.") %+% BDmetrics)
 ES_frag_group_df[,namES_df] <- NA
 
 for(i in 1:length(ES_frag_group_df$Case.ID)){
    sub.df <- subset(div_df, filename %in% ES_frag_group_df$Case.ID[i])
    small.df <- sub.df[which(sub.df$entity.size.rank<median(sub.df$entity.size.rank)),]
    large.df <- sub.df[which(sub.df$entity.size.rank>median(sub.df$entity.size.rank)),]
-   for(j in names(div_df)[-(1:3)]){
-      ES_frag_group_df[i,"n.fragment"] <- nrow(sub.df)
+   ES_frag_group_df[i,"n.fragment"] <- nrow(sub.df)
+   for(j in BDmetrics){
       ES_frag_group_df[i,"logRR." %+% j] <- log(mean(small.df[,j])/mean(large.df[,j]))
    }
 }
 
+###############################################################
 ### 3. Gradient of habitat fragmentation
+###############################################################
 #calculate rank-correlation
 ES_df <- data.frame(Case.ID=unique(div_df$filename))
-namES_df <- c("n.fragment",c("z.","z.var.") %+% names(div_df)[-(1:3)])
+namES_df <- c("n.fragment",c("z.","z.var.") %+% rep(BDmetrics,each=2))
 ES_df[,namES_df] <- NA
 
 for(i in 1:length(ES_df$Case.ID)){
    sub.df <- subset(div_df, filename==ES_df$Case.ID[i])
-   for(j in names(div_df)[-(1:3)]){
-      ES_df[i,"n.fragment"] <- nrow(sub.df)
+   ES_df[i,"n.fragment"] <- nrow(sub.df)
+   for(j in BDmetrics){
       r <- ifelse(ES_df[i,"n.fragment"] < 90,
                   2*sin(pi*cor(sub.df$entity.size.rank,sub.df[,j],method="spearman", use = "na.or.complete")/6),
                   cor(sub.df$entity.size.rank,sub.df[,j],method="spearman", use = "na.or.complete")) ### transform rank-correlation into Pearson-correlation, cf. Box 13.3. p 201 in Koricheva et al 2013
@@ -54,7 +58,9 @@ for(i in 1:length(ES_df$Case.ID)){
    }
 }
 
+###############################################################
 ### create unique study identifier
+###############################################################
 splits <- sapply(as.character(ES_df$Case.ID),function(x) strsplit(x,"_")[[1]])
 ES_frag_df$Study.ID <- ES_frag_group_df$Study.ID <- ES_df$Study.ID <- sapply(splits, function(x) paste(x[1], x[2], sep="_"))
 
