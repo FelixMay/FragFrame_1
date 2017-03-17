dd913572703579b6af9c444f7617ae64dcce0259
KatharinaGerstner <katharina.gerstner@idiv.de>
2017-03-17 13:30:34 +0100

    rename effect size colums to "ES"

--- 01.3_CalculateEffectSizes.r
+++ 01.3_CalculateEffectSizes.r
@@ -5,10 +5,10 @@ str(div_df)
 BDmetrics <- c("N_std","S","D0_hat","ENS_pie")
 
 ###############################################################
-### 1. largest vs smallest fragment incl continuous as fragment
+### 1. largest vs smallest fragment incl continuous as fragment using log RR
 ###############################################################
 ES_frag_df <- data.frame(Case.ID=unique(div_df$filename))
-namES_df <- c("n.fragment",c("logRR.") %+% BDmetrics)
+namES_df <- c("n.fragment",c("ES.") %+% BDmetrics)
 ES_frag_df[,namES_df] <- NA
 
 for(i in 1:length(ES_frag_df$Case.ID)){
@@ -17,15 +17,15 @@ for(i in 1:length(ES_frag_df$Case.ID)){
    large.df <- sub.df[which.max(sub.df$entity.size.rank),]
    ES_frag_df[i,"n.fragment"] <- nrow(sub.df)
    for(j in BDmetrics){
-      ES_frag_df[i,"logRR." %+% j] <- log(small.df[,j]/large.df[,j])
+      ES_frag_df[i,"ES." %+% j] <- log(small.df[,j]/large.df[,j])
    }
 }
 
 ###############################################################
-### 2. largest (incl. continuous) vs smallest fragment group
+### 2. largest (incl. continuous) vs smallest fragment group using log RR
 ###############################################################
 ES_frag_group_df <- data.frame(Case.ID=unique(div_df$filename))
-namES_df <- c("n.fragment",c("logRR.") %+% BDmetrics)
+namES_df <- c("n.fragment",c("ES.") %+% BDmetrics)
 ES_frag_group_df[,namES_df] <- NA
 
 for(i in 1:length(ES_frag_group_df$Case.ID)){
@@ -34,16 +34,16 @@ for(i in 1:length(ES_frag_group_df$Case.ID)){
    large.df <- sub.df[which(sub.df$entity.size.rank>median(sub.df$entity.size.rank)),]
    ES_frag_group_df[i,"n.fragment"] <- nrow(sub.df)
    for(j in BDmetrics){
-      ES_frag_group_df[i,"logRR." %+% j] <- log(mean(small.df[,j])/mean(large.df[,j]))
+      ES_frag_group_df[i,"ES." %+% j] <- log(mean(small.df[,j])/mean(large.df[,j]))
    }
 }
 
 ###############################################################
-### 3. Gradient of habitat fragmentation
+### 3. Gradient of habitat fragmentation using Fishers' z
 ###############################################################
 #calculate rank-correlation
 ES_df <- data.frame(Case.ID=unique(div_df$filename))
-namES_df <- c("n.fragment",c("z.","z.var.") %+% rep(BDmetrics,each=2))
+namES_df <- c("n.fragment",c("ES.","ES.var.") %+% rep(BDmetrics,each=2))
 ES_df[,namES_df] <- NA
 
 for(i in 1:length(ES_df$Case.ID)){
@@ -53,8 +53,8 @@ for(i in 1:length(ES_df$Case.ID)){
       r <- ifelse(ES_df[i,"n.fragment"] < 90,
                   2*sin(pi*cor(sub.df$entity.size.rank,sub.df[,j],method="spearman", use = "na.or.complete")/6),
                   cor(sub.df$entity.size.rank,sub.df[,j],method="spearman", use = "na.or.complete")) ### transform rank-correlation into Pearson-correlation, cf. Box 13.3. p 201 in Koricheva et al 2013
-      ES_df[i,"z." %+% j] <- 1/2*log((1+r)/(1-r))
-      ES_df[i,"z.var." %+% j] <- ifelse(ES_df[i,"n.fragment"]>3,1/(ES_df[i,"n.fragment"]-3),NA)
+      ES_df[i,"ES." %+% j] <- 1/2*log((1+r)/(1-r))
+      ES_df[i,"ES.var." %+% j] <- ifelse(ES_df[i,"n.fragment"]>3,1/(ES_df[i,"n.fragment"]-3),NA)
    }
 }
 
@@ -71,7 +71,7 @@ write.csv(ES_df, file=path2temp %+% "ES_df.csv")
 
 ### Check studies with no effect size
 ### Not needed for automated execution, just for error checking
-# (check <- ES_df[is.na(ES_df$z.S), ])
+# (check <- ES_df[is.na(ES_df$ES.S), ])
 # div_df[div_df$filename %in% check$Case.ID,  ]
 
 
