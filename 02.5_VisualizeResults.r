225a3227d968454d7fd24e5fd5ec5a373c377144
KatharinaGerstner <katharina.gerstner@idiv.de>
2017-05-09 10:51:54 +0200

    expand plotting

--- 02.5_VisualizeResults.r
+++ 02.5_VisualizeResults.r
@@ -1,86 +1,167 @@
 load(path2temp %+% "02.1_Data4Analysis_out.Rdata") 
 load(path2temp %+% "02.3_DataAnalysis_out.Rdata") # model_frag, model_frag_group, model_gradient
 
-theme_habfrag <- function (base_size = 12, base_family = "", rel.text.size=1.5, legend.position = "right") {
-   theme_grey(base_size = base_size, base_family = base_family) %+replace% 
-      theme(axis.text = element_text(size = rel(rel.text.size-0.2)), 
-            axis.ticks = element_line(colour = "black"), 
-            axis.title = element_text(size = rel(rel.text.size)), 
-            legend.text=element_text(size = rel(rel.text.size)),
-            legend.title=element_text(size = rel(rel.text.size)),
-            legend.key = element_rect(colour = "grey80"), 
-            legend.position = legend.position,
-            panel.background = element_rect(fill = "white", colour = NA), 
-            panel.border = element_rect(fill = NA, colour = "grey50"), 
-            panel.grid.major = element_line(colour = "grey90", size = 0.2), 
-            panel.grid.minor = element_line(colour = "grey98", size = 0.5), 
-            strip.background = element_rect(fill = "grey80", colour = "grey50", size = 0.2),
-            strip.text = element_text(size=rel(rel.text.size)))
-}
-
 plot.func <- function(model,ylab,title){
-   pred.se.list <- lapply(model,function(x) data.frame(b=x$b,se=x$se))
+   pred.se.list <- lapply(model,function(x) lapply(x, function(y) data.frame(b=y$b,se=y$se)))
 #   str(pred.se.list)
-   pred.se.df <- bind_rows(pred.se.list,.id="model")
-   pred.se.df$variable <- c("S", "D0_hat", "N_std", "ENS_pie")
+   pred.se.df.list <- lapply(pred.se.list, function(x) bind_rows(x,.id="model"))
+   pred.se.df <- bind_rows(pred.se.df.list)
+   pred.se.df$levels <- rep(c("frag","frag_group","gradient"),each=4)
+   pred.se.df$variable <- rep(c("S", "D0_hat", "N_std", "ENS_pie"),times=3)
    ylim <- c(floor(10*min(pred.se.df$b-1.96*pred.se.df$se))/10,ceiling(10*max(pred.se.df$b+1.96*pred.se.df$se))/10)
    
-   plot1 <- ggplot(pred.se.df) +
-      geom_point(aes(x=variable, y=b),size=4) +
-      geom_errorbar(aes(x=variable, ymin=b-1.96*se,ymax=b+1.96*se),width=0.2,size=1.2) +
-      geom_hline(yintercept=0,linetype="twodash", size=0.6) +
-      scale_x_discrete("",limits=c("S", "D0_hat", "N_std", "ENS_pie")) +
-      xlab("") + ylab(ylab) + ylim(ylim) +
-      ggtitle(title) +
-      theme_habfrag(rel.text.size=1.5)   
-   print(plot1)
-   
-}
-
-png(file=path2temp %+% "ResultsPlots/ResultPlot_frag.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
-plot.func(model=model_frag, ylab="Log(Response Ratio)",title="Smallest vs. largest fragment")
-dev.off()
-
-png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_group.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
-plot.func(model_frag_group, ylab="Log(Response Ratio)",title="Smallest vs. largest fragment group")
-dev.off()
-
-png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_gradient.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
-plot.func(model_gradient, ylab="Fishers' z",title="Gradient of fragmentation")
-dev.off()
-
-#####################################################################################
-plot.func.taxa <- function(model,ylab,title){
-   pred.se.list <- lapply(model,function(x) data.frame(b=x$b,se=x$se))
-   #   str(pred.se.list)
-   pred.se.df <- bind_rows(pred.se.list,.id="model")
-   pred.se.df$variable <- rep(c("S", "D0_hat", "N_std", "ENS_pie"),each=5)
-   pred.se.df$taxa <- c("invertebrates", "amphibians & reptiles", "birds", "mammals","plants") 
-   ylim <- c(floor(10*min(pred.se.df$b-1.96*pred.se.df$se))/10,ceiling(10*max(pred.se.df$b+1.96*pred.se.df$se))/10)
-   
-   pd <- position_dodge(width=0.6)
-   
-   plot1 <- ggplot(pred.se.df[!(pred.se.df$model == "Common vs. rare"),], aes(color=taxa)) +
+   pd <- position_dodge(width=0.4)
+   coul = brewer.pal(length(levels(pred.se.df$levels)), "Accent")    # Classic palette, with n.study colors
+   plot1 <- ggplot(pred.se.df, aes(color=levels)) +
       geom_point(position=pd,aes(x=variable, y=b),size=4) +
       geom_errorbar(position=pd,aes(x=variable, ymin=b-1.96*se,ymax=b+1.96*se),width=0.2,size=1.2) +
       geom_hline(yintercept=0,linetype="twodash", size=0.6) +
       scale_x_discrete("",limits=c("S", "D0_hat", "N_std", "ENS_pie")) +
       xlab("") + ylab(ylab) + ylim(ylim) +
       ggtitle(title) +
-      guides(color=F) +
-      theme_habfrag(rel.text.size=1.5)   
+      scale_color_manual("", values=coul,
+                         guide=guide_legend(title="", direction="horizontal",nrow=1)) +
+      theme_bw() +
+      theme(legend.position="bottom")
+   print(plot1)
+   
+}
+
+model <- list(model_frag[["GrandMean"]],model_frag_group[["GrandMean"]],model_gradient[["GrandMean"]])
+
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_GrandMean.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
+plot.func(model=model_frag[["GrandMean"]],ylab="Log(Response Ratio)",title="Smallest vs. largest fragment")
+dev.off()
+
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_group_GrandMean.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
+plot.func(model_frag_group[["GrandMean"]], ylab="Log(Response Ratio)",title="Smallest vs. largest fragment group")
+dev.off()
+
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_gradient_GrandMean.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
+plot.func(model_gradient[["GrandMean"]], ylab="Fishers' z",title="Gradient of fragmentation")
+dev.off()
+
+#####################################################################################
+plot.func.levels <- function(model,cov,levels,ylab,title){
+   pred.se.list <- lapply(model,function(x) data.frame(b=x$b,se=x$se))
+   pred.se.df <- bind_rows(pred.se.list,.id="model")  
+   pred.se.df$variable <- rep(c("S", "D0_hat", "N_std", "ENS_pie"),each=length(levels))
+   pred.se.df$levels <- unlist(lapply(model, function(x) sapply(strsplit(rownames(x$b),cov), function(y) y[2])))
+   pred.se.df$levels <- factor(pred.se.df$levels, levels=levels) 
+   
+   ylim <- c(floor(10*min(pred.se.df$b-1.96*pred.se.df$se))/10,ceiling(10*max(pred.se.df$b+1.96*pred.se.df$se))/10)
+   
+   pd <- position_dodge(width=0.6)
+   coul = brewer.pal(length(levels), "Accent")    # Classic palette, with n.study colors
+   
+   plot1 <- ggplot(pred.se.df, aes(color=levels)) +
+      geom_point(position=pd,aes(x=variable, y=b),size=4) +
+      geom_errorbar(position=pd,aes(x=variable, ymin=b-1.96*se,ymax=b+1.96*se),width=0.2,size=1.2) +
+      geom_hline(yintercept=0,linetype="twodash", size=0.6) +
+      scale_x_discrete("",limits=c("S", "D0_hat", "N_std", "ENS_pie")) +
+      xlab("") + ylab(ylab) + ylim(ylim) +
+      ggtitle(title) +
+      scale_color_manual("", values=coul,
+                         guide=guide_legend(title="", direction="horizontal",nrow=round(length(levels)/3))) +
+      theme_bw() +
+      theme(legend.position="bottom")
    
    print(plot1)
 }
 
-png(file=path2temp %+% "ResultsPlots/ResultPlot_Taxafrag.png", width=30,height=10,units="cm",res=200,type = "cairo-png")
-plot.func.taxa(modelTaxa_frag, ylab="Log(Response Ratio)",title="Smallest vs. largest fragment")
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_Taxa.png", width=30,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.levels(model=model_frag[["Taxa"]],cov="taxa", levels=c("invertebrates", "amphibians & reptiles", "birds", "mammals","plants"), ylab="Log(Response Ratio)",title="Smallest vs. largest fragment")
 dev.off()
 
-png(file=path2temp %+% "ResultsPlots/ResultPlot_Taxafrag_group.png", width=30,height=10,units="cm",res=200,type = "cairo-png")
-plot.func.taxa(modelTaxa_frag_group, ylab="Log(Response Ratio)",title="Smallest vs. largest fragment group")
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_Biome.png", width=30,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.levels(model=model_frag[["Biome"]],cov="biome", levels=levels(ES_frag_df.complete$biome), ylab="Log(Response Ratio)",title="Smallest vs. largest fragment")
 dev.off()
 
-png(file=path2temp %+% "ResultsPlots/ResultPlot_Taxafrag_gradient.png", width=30,height=10,units="cm",res=200,type = "cairo-png")
-plot.func.taxa(modelTaxa_gradient, ylab="Fishers' z",title="Gradient of fragmentation")
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_Matrix.png", width=30,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.levels(model=model_frag[["Matrix"]],cov="matrix.category", levels=c("light filter", "medium filter", "harsh filter"), ylab="Log(Response Ratio)",title="Smallest vs. largest fragment")
 dev.off()
+
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_Time.png", width=30,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.levels(model=model_frag[["Time"]],cov="time.since.fragmentation", levels=c("recent (less than 20 years)", "intermediate (20-100 years)", "long (100+ years)"), ylab="Log(Response Ratio)",title="Smallest vs. largest fragment")
+dev.off()
+
+##
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_group_Taxa.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.levels(model_frag_group[["Taxa"]],cov="taxa", levels=c("invertebrates", "amphibians & reptiles", "birds", "mammals","plants"), ylab="Log(Response Ratio)",title="Smallest vs. largest fragment group")
+dev.off()
+
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_group_Biome.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.levels(model_frag_group[["Biome"]],cov="biome", levels=levels(ES_frag_df.complete$biome), ylab="Log(Response Ratio)",title="Smallest vs. largest fragment group")
+dev.off()
+
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_group_Matrix.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.levels(model_frag_group[["Matrix"]],cov="matrix.category", levels=c("light filter", "medium filter", "harsh filter"), ylab="Log(Response Ratio)",title="Smallest vs. largest fragment group")
+dev.off()
+
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_group_Time.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.levels(model_frag_group[["Time"]],cov="time.since.fragmentation", levels=c("recent (less than 20 years)", "intermediate (20-100 years)", "long (100+ years)"), ylab="Log(Response Ratio)",title="Smallest vs. largest fragment group")
+dev.off()
+
+##
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_gradient_Taxa.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.levels(model_gradient[["Taxa"]],cov="taxa", levels=c("invertebrates", "amphibians & reptiles", "birds", "mammals","plants"), ylab="Fishers' z",title="Gradient of fragmentation")
+dev.off()
+
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_gradient_Biome.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.levels(model_gradient[["Biome"]],cov="biome", levels=levels(ES_frag_df.complete$biome), ylab="Fishers' z",title="Gradient of fragmentation")
+dev.off()
+
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_gradient_Matrix.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.levels(model_gradient[["Matrix"]],cov="matrix.category", levels=c("light filter", "medium filter", "harsh filter"), ylab="Fishers' z",title="Gradient of fragmentation")
+dev.off()
+
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_gradient_Time.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.levels(model_gradient[["Time"]],cov="time.since.fragmentation", levels=c("recent (less than 20 years)", "intermediate (20-100 years)", "long (100+ years)"), ylab="Fishers' z",title="Gradient of fragmentation")
+dev.off()
+
+
+
+#####################################################################################
+plot.func.continuous <- function(model,newmods,ylab,title){
+   pred.se.list <- lapply(model,function(x) predict(x,newmods=newmods))
+   #   str(pred.se.list)
+   pred.list2df <- function(pred.list){
+      pred.df <- data.frame(pred=pred.list$pred,
+                            se=pred.list$se,
+                            ci.lb=pred.list$ci.lb,
+                            ci.ub=pred.list$ci.ub,
+                            cr.lb=pred.list$cr.lb,
+                            cr.ub=pred.list$cr.ub)
+      return(pred.df)
+   }
+   pred.se.df <- bind_rows(lapply(pred.se.list,pred.list2df),.id="model")
+   
+   pred.se.df$variable <- factor(rep(c("S", "D0_hat", "N_std", "ENS_pie"),each=length(newmods)),levels=c("S", "D0_hat", "N_std", "ENS_pie"))
+   pred.se.df$newmods <- newmods 
+
+   plot1 <- ggplot(pred.se.df,aes(y=pred,x=newmods)) +
+      geom_line(size=2) +
+      geom_hline(yintercept=0,linetype="twodash", size=0.6) +
+      geom_ribbon(aes(ymin=ci.lb,ymax=ci.ub),alpha=0.3,color=NA,fill="grey") +
+      xlab("Min/Max Size Ratio") + ylab(ylab) +
+      ggtitle(title) +
+      facet_grid(variable~.) +
+      theme_bw() +
+      theme(legend.position="bottom")
+   
+   print(plot1)
+}
+
+
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_SizeRatio.png", width=30,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.continuous(model=model_frag[["SizeRatio"]], newmods=seq(min(ES_frag_df.complete$ratio.min.max.fragment.size2,na.rm=T),max(ES_frag_df.complete$ratio.min.max.fragment.size2,na.rm=T),by=0.05), ylab="Log(Response Ratio)",title="Smallest vs. largest fragment")
+dev.off()
+
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_group_SizeRatio.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.continuous(model_frag_group[["SizeRatio"]], newmods=seq(min(ES_frag_df.complete$ratio.min.max.fragment.size2,na.rm=T),max(ES_frag_df.complete$ratio.min.max.fragment.size2,na.rm=T),by=0.05), ylab="Log(Response Ratio)",title="Smallest vs. largest fragment group")
+dev.off()
+
+png(file=path2temp %+% "ResultsPlots/ResultPlot_frag_gradient_SizeRatio.png", width=20,height=10,units="cm",res=200,type = "cairo-png")
+plot.func.continuous(model_gradient[["SizeRatio"]], newmods=seq(min(ES_frag_df.complete$ratio.min.max.fragment.size2,na.rm=T),max(ES_frag_df.complete$ratio.min.max.fragment.size2,na.rm=T),by=0.05), ylab="Fishers' z",title="Gradient of fragmentation")
+dev.off()
+
