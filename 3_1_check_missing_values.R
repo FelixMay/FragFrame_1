d49e5bfe8340f736405344dfb763cb92c095e058
FelixMay <felix.may@leuphana.de>
2019-04-11 12:27:17 +0200

    Check NA values

---
+++ 3_1_check_missing_values.R
@@ -0,0 +1,29 @@
+infile <- path2Dropbox %+% "/files_datapaper/Analysis/2_biodiv_frag_fcont_10_mabund_as_is.csv"
+frag_div <- read.csv(infile, stringsAsFactors = F)
+
+summary(frag_div)
+
+na_S_std_1 <- frag_div %>%
+   filter(is.na(S_std_1)) 
+summary(na_S_std_1)
+# seems to be OK --> N_std is always smaller than 5 and than rarefaction does not work
+
+na_S_std_2 <- frag_div %>%
+   filter(is.na(S_std_2)) 
+summary(na_S_std_2)
+# same just for the pooled sampling designs (in the other ones rarefaction is not used)
+
+na_S_PIE <- frag_div %>%
+   filter(is.na(S_PIE)) 
+summary(na_S_PIE)
+dim(na_S_PIE)
+select(na_S_PIE, N, S_obs)
+# S == N!
+
+na_S_n <- frag_div %>%
+   filter(is.na(S_n)) 
+na_S_n
+summary(na_S_n)
+
+frag_div %>% filter(dataset_label == "Ewers_2007")
+
