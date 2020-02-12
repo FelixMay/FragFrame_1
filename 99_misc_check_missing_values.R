68290f247c1329e34aec25f101e36d8a49d09a1a
FelixMay <felix.may@leuphana.de>
2020-02-12 17:22:10 +0100

    Check fragments where S_PIE > S_obs

--- 99_misc_check_missing_values.R
+++ 99_misc_check_missing_values.R
@@ -1,13 +1,31 @@
 infile <- path2Dropbox %+% "/files_datapaper/Analysis/2_biodiv_frag_fcont_10_mabund_as_is.csv"
 frag_div <- read.csv(infile, stringsAsFactors = F)
 
+# Check the abundance distribution in fragments where S_PIE > S_obs
+infile <- path2Dropbox %+% "files_datapaper/Long_format_database/fragSAD_predicts_ewers.csv"
+abund_dat <- read.csv(infile, stringsAsFactors = F)
+
+s_pie_large <- frag_div %>% 
+   filter(S_PIE_mean > S_obs)
+plot(S_PIE_mean ~ S_obs, data = frag_div, log = "xy")
+abline(0,1)
+
+abund_s_pie_large <- abund_dat %>%
+   inner_join(s_pie_large) %>%
+   select(dataset_label, frag_id, sample_id, species, abundance, N, S_obs, S_PIE_mean)
+dim(abund_s_pie_large)
+View(abund_s_pie_large)
+
+
+
 # Get numbers of fragments per study
-frag_div %>%
+ frag_div %>%
    select(dataset_label, frag_id) %>%
    distinct() %>% 
    group_by(dataset_label) %>%
    count() %>% 
    arrange(desc(n))
+
 
 summary(frag_div)
 
